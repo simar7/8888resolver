50a262555fbaf09df2ca5dc14edd75d2817b3d28
Simarpreet Singh <simar@linux.com>
2018-04-08 14:19:57 -0700

    main_test: Add table driven tests
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- main_test.go
+++ main_test.go
@@ -13,46 +13,118 @@ import (
 	gock "gopkg.in/h2non/gock.v1"
 )
 
-func TestGetDNS_Type_A(t *testing.T) {
+func TestGetDNS(t *testing.T) {
 	defer gock.Off()
 
-	testDNSRecord := DNSResponse{
-		Answers: []DNSAnswer{
-			{
-				TTL:  42,
-				Data: "2607:dead:face:cafe8::200e",
-				Name: "example.com.",
-				Type: dns.TypeAAAA,
+	testCases := []struct {
+		Name             string
+		TestDNSRecord    DNSResponse
+		ExpectedError    error
+		ExpectedResponse DNSResponse
+		DNSURL           string
+		RequestURL       string
+	}{
+		{
+			Name: "Get Type AAAA",
+			TestDNSRecord: DNSResponse{
+				Answers: []DNSAnswer{
+					{
+						TTL:  42,
+						Data: "2607:dead:face:cafe8::200e",
+						Name: "example.com.",
+						Type: dns.TypeAAAA,
+					},
+				},
+				Questions: []DNSQuestion{
+					{
+						Name: "example.com.",
+						Type: dns.TypeAAAA,
+					},
+				},
 			},
+			ExpectedError: nil,
+			ExpectedResponse: DNSResponse{
+				Answers: []DNSAnswer{
+					{
+						TTL:  42,
+						Data: "2607:dead:face:cafe8::200e",
+						Name: "example.com.",
+						Type: dns.TypeAAAA,
+					},
+				},
+				Questions: []DNSQuestion{
+					{
+						Name: "example.com.",
+						Type: dns.TypeAAAA,
+					},
+				},
+			},
+			DNSURL:     fmt.Sprintf("%s?name=%s&type=%s", GoogleDNS, "example.com", "AAAA"),
+			RequestURL: fmt.Sprintf("/dns/example.com/AAAA"),
 		},
-		Questions: []DNSQuestion{
-			{
-				Name: "example.com.",
-				Type: dns.TypeAAAA,
+
+		{
+			Name: "Get Type A",
+			TestDNSRecord: DNSResponse{
+				Answers: []DNSAnswer{
+					{
+						TTL:  42,
+						Data: "8.8.8.8",
+						Name: "example.com.",
+						Type: dns.TypeA,
+					},
+				},
+				Questions: []DNSQuestion{
+					{
+						Name: "example.com.",
+						Type: dns.TypeA,
+					},
+				},
 			},
+			ExpectedError: nil,
+			ExpectedResponse: DNSResponse{
+				Answers: []DNSAnswer{
+					{
+						TTL:  42,
+						Data: "8.8.8.8",
+						Name: "example.com.",
+						Type: dns.TypeA,
+					},
+				},
+				Questions: []DNSQuestion{
+					{
+						Name: "example.com.",
+						Type: dns.TypeA,
+					},
+				},
+			},
+			DNSURL:     fmt.Sprintf("%s?name=%s&type=%s", GoogleDNS, "example.com", "A"),
+			RequestURL: fmt.Sprintf("/dns/example.com/A"),
 		},
 	}
-	gockURL := fmt.Sprintf("%s?name=%s&type=%s", GoogleDNS, "example.com", "AAAA")
-	gock.New(gockURL).Reply(http.StatusOK).JSON(testDNSRecord)
 
-	ro := Router{
-		HTTPClient: newHTTPClient(time.Second * 10),
+	for _, tc := range testCases {
+		gock.New(tc.DNSURL).Reply(http.StatusOK).JSON(tc.TestDNSRecord)
+
+		ro := Router{
+			HTTPClient: newHTTPClient(time.Second * 10),
+		}
+		router := ro.setupRouter()
+
+		w := httptest.NewRecorder()
+		req, err := http.NewRequest(http.MethodGet, tc.RequestURL, nil)
+		assert.NoError(t, err, tc.Name)
+
+		router.ServeHTTP(w, req)
+
+		expected := tc.ExpectedResponse
+		actual := DNSResponse{}
+		err = json.Unmarshal(w.Body.Bytes(), &actual)
+		assert.NoError(t, err, tc.Name)
+
+		assert.Equal(t, http.StatusOK, w.Code, tc.Name)
+		assert.Equal(t, expected, actual, tc.Name)
 	}
-	router := ro.setupRouter()
-
-	w := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/dns/example.com/AAAA", nil)
-	assert.NoError(t, err)
-
-	router.ServeHTTP(w, req)
-
-	expected := testDNSRecord
-	actual := DNSResponse{}
-	err = json.Unmarshal(w.Body.Bytes(), &actual)
-	assert.NoError(t, err)
-
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, expected, actual)
 
 	// Verify that we don't have pending mocks
 	assert.True(t, gock.IsDone())
